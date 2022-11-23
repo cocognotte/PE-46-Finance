03fa9f80239a8a5e8a4b4ca22bdc9451feb3d2ae
Elouan <elouan81@gmail.com>
2022-11-23 21:11:11 +0100

    first commit

---
+++ Scraping.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 16 15:16:20 2022
+
+@author: eloua
+"""
+
+import pandas as pd
+from bs4 import  BeautifulSoup as bs
+import urllib.request
+
+
+url='https://www.boursorama.com/bourse/indices/cours/1rPCAC/'
+
+page=urllib.request.urlopen(url,timeout=5)
+
+soup=bs(page,features="lxml")         
+
+soup
