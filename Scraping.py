f65f18b1069125e6b1b8d6925b7fbce4f0a48200
Thomas <thomasulpice@gmail.com>
2022-11-28 20:51:11 +0100

    programme test sur DJI sur l'année passée

--- Scraping.py
+++ Scraping.py
@@ -1,21 +1,50 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 16 15:16:20 2022
+# code elouan
 
-@author: eloua
-"""
+# import pandas as pd
+# from bs4 import  BeautifulSoup as bs
+# import urllib.request
 
-import pandas as pd
-from bs4 import  BeautifulSoup as bs
+
+# url='https://www.boursorama.com/bourse/indices/cours/1rPCAC/'
+
+# page=urllib.request.urlopen(url,timeout=5)
+
+# soup=bs(page,features="lxml")
+
+
+import requests
+import bs4
 import urllib.request
 
+url = 'https://fr.finance.yahoo.com/quote/%5EDJI/history?p=%5EDJI'
 
-url='https://www.boursorama.com/bourse/indices/cours/1rPCAC/'
+request = requests.get(url, headers = {'User-Agent':'Mozilla/.0'})
 
-page=urllib.request.urlopen(url,timeout=5)
+request_text = request.text
+#print(request_text)
 
-soup=bs(page,features="lxml")
+page = bs4.BeautifulSoup(request_text , "lxml")
+#print(page)
 
-soup
+def span_convert(bs):
+    a = str(bs)[6:-7]
+    if '\u202f' in a:
+        a = a.replace('\u202f' , '')
+        a = a.replace(',' , '.')
+        return float(a)
+    else:
+        return a
 
-#teste
+L = [[]]
+c = 0
+
+for item in page.find("table" , {'data-test' : "historical-prices"}).findAll('span')[7:-3]:
+    if c%7 == 0:
+        L.append([span_convert(item)])
+    else:
+        L[c//7].append(span_convert(item))
+    c += 1
+    
+print(L)
+
+
